9b5f00f8dddefdf7ad053d8aebad6fcbbaa0652e
Illia Khovanskyi <saskiuhia@gmail.com>
2024-01-11 22:12:42 +0100

    Removed unit tests project

--- IVAXOR.PatreonNET.UnitTests/Usings.cs
+++
@@ -1,7 +0,0 @@
-global using System.Net;
-
-global using Microsoft.VisualStudio.TestTools.UnitTesting;
-
-global using Moq;
-global using Moq.Protected;
-global using IVAXOR.PatreonNET.Services.TokenManagers.Interfaces;
